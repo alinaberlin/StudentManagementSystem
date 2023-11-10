56a66aa0d0838ff214fd6412a9fb2eec73747f98
alinaberlin <alina.ghetler@gmail.com>
2023-11-10 16:57:50 +0100

    Add CRUD for teachers and students.

--- src/main/java/com/github/alina/services/StudentService.java
+++ src/main/java/com/github/alina/services/StudentService.java
@@ -3,6 +3,8 @@ package com.github.alina.services;
 import com.github.alina.models.Student;
 
 import java.io.*;
+import java.util.ArrayList;
+import java.util.List;
 
 public class StudentService {
 
@@ -36,6 +38,49 @@ public class StudentService {
             throw new RuntimeException(e);
         }
     }
+
+    public Student update(Student student) {
+        List<Student> students = new ArrayList<>();
+        try (
+                FileInputStream fileInputStream = new FileInputStream("student.out");
+                ObjectInputStream objectInputStream = new ObjectInputStream(fileInputStream)) {
+            while (true) {
+                Student existing = (Student) objectInputStream.readObject();
+                if (student.getId() == existing.getId()) {
+                    students.add(student);
+                } else {
+                    students.add(existing);
+                }
+            }
+        } catch (IOException e) {
+
+        } catch (ClassNotFoundException e) {
+        }
+        for (Student s : students) {
+            insert(s);
+        }
+        return student;
+    }
+
+    public void delete(int id) throws FileNotFoundException {
+        List<Student> students = new ArrayList<>();
+        try (FileInputStream fileInputStream = new FileInputStream("student.out");
+             ObjectInputStream objectInputStream = new ObjectInputStream(fileInputStream)) {
+            while (true) {
+                Student existing = (Student) objectInputStream.readObject();
+                if (existing.getId() != id) {
+                    students.add(existing);
+                }
+            }
+
+        } catch (IOException e) {
+
+        } catch (ClassNotFoundException e) {
+            throw new RuntimeException(e);
+        }
+        students.forEach(s -> insert(s));
+    }
+
 }
 
 
--- src/main/java/com/github/alina/services/TeacherService.java
+++ src/main/java/com/github/alina/services/TeacherService.java
@@ -1,8 +1,11 @@
 package com.github.alina.services;
 
+import com.github.alina.models.Student;
 import com.github.alina.models.Teacher;
 
 import java.io.*;
+import java.util.ArrayList;
+import java.util.List;
 
 public class TeacherService {
     public TeacherService() {
@@ -35,6 +38,47 @@ public class TeacherService {
             throw new RuntimeException(e);
         }
 
+    }
+
+    public Teacher update(Teacher teacher) {
+        List<Teacher> teachers = new ArrayList<>();
+        try (
+                FileInputStream fileInputStream = new FileInputStream("teacher.out");
+                ObjectInputStream objectInputStream = new ObjectInputStream(fileInputStream)) {
+            while (true) {
+                Teacher existing = (Teacher) objectInputStream.readObject();
+                if (teacher.getId() == existing.getId()) {
+                    teachers.add(teacher);
+                } else {
+                    teachers.add(existing);
+                }
+            }
+        } catch (IOException e) {
+
+        } catch (ClassNotFoundException e) {
+        }
+
+        teachers.forEach(t -> insert(t));
+        return teacher;
+    }
+
+    public void delete(int id) throws FileNotFoundException {
+        List<Teacher> teachers = new ArrayList<>();
+        try (FileInputStream fileInputStream = new FileInputStream("teacher.out");
+             ObjectInputStream objectInputStream = new ObjectInputStream(fileInputStream)) {
+            while (true) {
+                Teacher existing = (Teacher) objectInputStream.readObject();
+                if (existing.getId() != id) {
+                    teachers.add(existing);
+                }
+            }
+
+        } catch (IOException e) {
+
+        } catch (ClassNotFoundException e) {
+            throw new RuntimeException(e);
+        }
+        teachers.forEach(t -> insert(t));
     }
 }
 
